7f49627417a616ec6c561e21724689d8a8049982
kkkkkkkadzi <104793706+kkkkkkkadzi@users.noreply.github.com>
2026-02-14 13:53:07 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,12 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Valentines: A Romantic Card Game with a Surprise Proposal",
+  title: "Игруля для любимого айпадкида",
   description:
-    "Play a unique Valentine's card game. Complete the collection to reveal a romantic proposal!",
+    "Быстро заходи!!!",
   keywords: [
-    "Valentine's card game",
-    "romantic proposal game",
-    "photo card challenge",
-    "Valentine's Day surprise",
-    "couples game",
-    "valentine's day game",
-    "proposal game",
+    "Игра на день святого валентина",
+    "романтическая игра",
   ],
 };
 
