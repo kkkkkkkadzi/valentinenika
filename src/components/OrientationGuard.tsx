daf99a878a04ac381f8db2449ae292a25021e157
kkkkkkkadzi <104793706+kkkkkkkadzi@users.noreply.github.com>
2026-02-14 14:06:31 +0200

    Update OrientationGuard.tsx

--- src/components/OrientationGuard.tsx
+++ src/components/OrientationGuard.tsx
@@ -46,11 +46,11 @@ export default function OrientationGuard({
           </svg>
         </motion.div>
         <h2 className="text-3xl font-bold text-white mb-4">
-          Rotate your device
+          Котку переверни телефон
         </h2>
         <p className="text-gray-400 text-lg max-w-xs">
-          To be able to play correctly, you need to put your phone in{" "}
-          <b>landscape mode</b>.
+          Тебе нужно чуть{" "}
+          <b>повернуть его</b>.
         </p>
       </div>
     );
